c8b6ae5c261de78874dccffe4cde79561bd42168
Jake Swackhammer <j.100101@gmail.com>
2021-10-30 13:34:45 -0400

    added sessions and cookies implementation

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const path = require('path');
 const logger = require('morgan');
 // eslint-disable-next-line no-unused-vars
 const createError = require('http-errors');
-
 const indexRouter = require('./routes/index');
+
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
 
 const app = express();
 const port = process.env.PORT || '3001'
@@ -14,16 +19,33 @@ const port = process.env.PORT || '3001'
 app.engine('hbs', expressHandlebars())
 app.set('view engine', 'hbs'); //FIXME
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.listen(port, ()=>{
-  console.log(`listening on http://localhost:3001/`)
-})
+const init = async () => {
+  await sequelize.sync({ force: false })
+  app.listen(port, ()=>{
+    console.log(`listening on http://localhost:3001/`)
+  })
+}
+
+init()
+
 
 module.exports = app;
--- routes/home-routes.js
+++ routes/home-routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.send(`you have the homepage now`);
     res.render('index', { title: 'Express' });
   });
 
