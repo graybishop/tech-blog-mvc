038ac5a12b3c71a8a0db60368aef11f778a7c618
Jake Swackhammer <j.100101@gmail.com>
2021-10-30 12:48:58 -0400

    added the rest of the API routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const usersRouter = require('./users.js');
+const usersRouter = require('./user-routes.js');
+const postRouter = require('./post-routes.js');
+const commentRouter = require('./comment-routes.js');
 
-router.use('/users', usersRouter)
+router.use('/user', usersRouter)
+router.use('/post', postRouter)
+router.use('/comment', commentRouter)
 
 module.exports = router
