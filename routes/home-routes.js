42b25fe00516b86f49fd29372c2316efbb7440a4
Jake Swackhammer <j.100101@gmail.com>
2021-10-30 17:59:42 -0400

    added blog posts and nav bar to home page

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('home-page', {
+    title: 'The Home Page',
+    blog: [
+      { title: 'test title 1', description: `test description 1`, author: `Boba` },
+      { title: 'test title two', description: `test description two`, author: `Lewis` }
+    ],
+    loggedIn: true
   });
+});
 
 module.exports = router;
