f23a90efbb1c023831df41ea435672cc45fe7f86
Jake Swackhammer <j.100101@gmail.com>
2021-10-30 20:54:10 -0400

    added model, and post route

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,11 +2,12 @@ const newFormHandler = async (event) => {
     event.preventDefault();
 
     const text = document.querySelector('#comment').value.trim();
+    const postId = document.location.pathname.split('/').pop()
 
     if (text) {
         const response = await fetch(`/api/comments`, {
             method: 'POST',
-            body: JSON.stringify({ text }),
+            body: JSON.stringify({ text, postId }),
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -22,6 +23,6 @@ const newFormHandler = async (event) => {
 
 window.addEventListener('DOMContentLoaded', () => {
     document
-        .querySelector('.new-post-form')
+        .querySelector('#commentForm')
         .addEventListener('submit', newFormHandler);
 });
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { Comment } = require('../../models');
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.post('/', async (req, res) => {
+
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+    return;
+  }
+
+  try {
+    const newProject = await Comment.create({
+      ...req.body,
+      userId: req.session.userId,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
