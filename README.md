a0bd4a7f53bd305e77af3577105fc776d7ce1114
Jake Swackhammer <j.100101@gmail.com>
2021-10-31 16:11:08 -0400

    fixed nav buttons

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 
 ## Link to Demo
 
+[Link to demo deployment on heroku.](https://ghastly-ghoul-76054.herokuapp.com/)
+
 ## Screenshots
 
 ## Tech Stack
---
+++ public/js/nav.js
@@ -0,0 +1,14 @@
+document.querySelector('#homeButton').addEventListener('click', () =>{
+    location.href = '/'
+})
+document.querySelector('#dashButton').addEventListener('click', () =>{
+    location.href = '/dashboard'
+})
+
+let loginBtn = document.querySelector('#loginButton')
+
+if (loginBtn){
+    loginBtn.addEventListener('click', () =>{
+        location.href = '/login'
+    })
+}
