441aa358387c7f4d87c6379fdcd1db7e70a0a509
Jake Swackhammer <j.100101@gmail.com>
2021-10-31 13:24:02 -0400

    cleaned up routes folder

--- routes/dashboard/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardRoutes = require('./dashboard-routes.js');
-
-router.use('/', dashboardRoutes)
-
-module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+
 const homeRouter = require('./home-routes.js');
+const dashboardRouter = require('./dashboard-routes.js');
+
 const apiRouter = require('./api/index.js');
-const dashboardRouter = require('./dashboard/index.js');
 
 router.use('/', homeRouter)
 router.use('/dashboard', dashboardRouter)
